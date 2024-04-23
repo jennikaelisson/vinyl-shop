f386f0efec68a46cbf683cbfaa7fd10ed25a5431
Jennika Elisson <jennika.elisson@gmail.com>
2024-04-23 17:09:21 +0200

    init commit new client

--- vite/main.tsx
+++ vite/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 
 const App = () => {
-    return <div>New</div>;
+    return <div><ProductList /></div>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
