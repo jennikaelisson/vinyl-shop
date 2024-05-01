ffef94677d289b10778669565078d3be348f9d05
Jennika Elisson <jennika.elisson@gmail.com>
2024-05-01 18:55:26 +0200

    updated client to take registration of customer

--- client/src/pages/CustomerRegister.tsx
+++ client/src/pages/CustomerRegister.tsx
@@ -1,9 +1,11 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 
 const CustomerRegister = () => {
+    const [firstNameInput, setFirstNameInput] = useState("");
+    const [lastNameInput, setLastNameInput] = useState("");
     const [emailInput, setEmailInput] = useState("");
-    const [passwordInput, setPasswordInput] = useState("");
-    const [nameInput, setNameInput] = useState("");
+    const [streetInput, setStreetInput] = useState("");
+    const [cityInput, setCityInput] = useState("");
 
     const register = async (e: FormEvent) => {
         e.preventDefault();
@@ -12,48 +14,76 @@ const CustomerRegister = () => {
           headers: {
             "Content-type": "application/json",
           },
-          body: JSON.stringify({ name: nameInput, email: emailInput, password: passwordInput }),
+          body: JSON.stringify({ 
+            firstName: firstNameInput,
+            lastName: lastNameInput,
+            email: emailInput,
+            address: {
+              street: streetInput,
+              city: cityInput
+            }
+          }),
         });
         const data = await response.json();
         console.log(data);
     
       };
 
-      const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setNameInput(e.target.value);
-        console.log(nameInput);
-      }
+      const handleFirstNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFirstNameInput(e.target.value);
+      };
+
+      const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setLastNameInput(e.target.value);
+      };
     
       const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
         setEmailInput(e.target.value);
-        console.log(emailInput);
       };
     
-      const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setPasswordInput(e.target.value);        
-        console.log(passwordInput);
+      const handleStreetChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStreetInput(e.target.value);
+      };
+
+      const handleCityChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCityInput(e.target.value);
       };
       
-    return <><form onSubmit={register}>
-    <label htmlFor="">Name</label>
-    <input
-      id="nameInput"
-      value={nameInput}
-      onChange={handleNameChange}
-    />
-    <label htmlFor="emailInput">Email</label>
-    <input
-      id="emailInput"
-      value={emailInput}
-      onChange={handleEmailChange}
-    />
-    <label htmlFor="passwordInput">Password</label>
-    <input
-      id="passwordInput"
-      value={passwordInput}
-      onChange={handlePasswordChange}
-    />
-    <button>Registrera</button>
-  </form></>
-}
+    return (
+        <form onSubmit={register}>
+            <label htmlFor="firstNameInput">First Name</label>
+            <input
+                id="firstNameInput"
+                value={firstNameInput}
+                onChange={handleFirstNameChange}
+            />
+            <label htmlFor="lastNameInput">Last Name</label>
+            <input
+                id="lastNameInput"
+                value={lastNameInput}
+                onChange={handleLastNameChange}
+            />
+            <label htmlFor="emailInput">Email</label>
+            <input
+                id="emailInput"
+                value={emailInput}
+                onChange={handleEmailChange}
+            />
+            <label htmlFor="streetInput">Street</label>
+            <input
+                id="streetInput"
+                value={streetInput}
+                onChange={handleStreetChange}
+            />
+            <label htmlFor="cityInput">City</label>
+            <input
+                id="cityInput"
+                value={cityInput}
+                onChange={handleCityChange}
+            />
+            <button>Register</button>
+        </form>
+    );
+};
+
 export default CustomerRegister;
